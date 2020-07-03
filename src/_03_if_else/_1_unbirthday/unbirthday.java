c788e9fae245bbb8fd614eced4243e120bc65326
root <root@09bf14ae66a2>
2020-07-03 20:36:00 +0000

    unbirthday

---
+++ src/_03_if_else/_1_unbirthday/unbirthday.java
@@ -0,0 +1,16 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class unbirthday {
+public static void main(String[] args) {
+	String response = JOptionPane.showInputDialog("when is your birthday?");
+	
+	// if( response.equals( "hello" ){
+	 if( response.equals ("july 3")  ) {
+		 JOptionPane.showMessageDialog(null, "happy birthday");
+	 }
+	
+	
+}
+}
